871ccc6a5d863398fcc7da61aa36f0eee68be8b9
Fatima Hammami <en.f.h@live.com>
2017-04-11 14:22:40 +0300

    route

--- routes.js
+++ routes.js
@@ -1,7 +1,40 @@
 
 module.exports=function(app,express){
-	app.get('/auth/facebook/callback',function(req,res){
-		console.log("its a request");
-		res.status(200).send();
+
+	// app.get('/auth/facebook/callback',function(req,res){
+	// 	console.log("its a request 1");
+	// 	res.status(200).send();
+	// })
+
+	// app.get('/auth/facebook/callback/:verify_token',function(req,res){
+	// 	console.log(req.params);
+	// 	var token="rebootkamp"
+	// 	if(req.params.verify_token === token
+	// 		res.status(200).send(req.params.hubmode);
+	// 	}else{
+	// 		res.status(500).send();
+	// 	}
+	// })
+
+	app.get('/auth/facebook/callback/:verify_token',function(req,res){
+		console.log(req.params.hubmode);
+		var token="rebootkamp"
+		if(req.params.verify_token === token){
+			res.status(200).send();
+		}else{
+			res.status(500).send();
+		}
 	})
+
+	// app.get('/auth/facebook/callback/:hub.challenge',function(req,res){
+	// 	console.log("its a request 2");
+	// 	res.status(200).send();
+	// })
+
+	// app.get('/auth/facebook/callback/:hub.verify_token',function(req,res){
+	// 	var token="rebootkamp";
+	// 	console.log(req.params);
+
+	// 	res.status(200).send();
+	// })
 }
