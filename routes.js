5b40c0cf62dd1c62ca8bbc29862ba5ca8fe9e57d
Fatima Hammami <en.f.h@live.com>
2017-04-12 09:32:52 +0300

    fix route

--- routes.js
+++ routes.js
@@ -1,41 +1,13 @@
 
 module.exports=function(app,express){
-
-	// app.get('/auth/facebook/callback',function(req,res){
-	// 	console.log("its a request 1");
-	// 	res.status(200).send();
-	// })
-
-	// app.get('/auth/facebook/callback/:verify_token',function(req,res){
-	// 	console.log(req.params);
-	// 	var token="rebootkamp"
-	// 	if(req.params.verify_token === token
-	// 		res.status(200).send(req.params.hubmode);
-	// 	}else{
-	// 		res.status(500).send();
-	// 	}
-	// })
-
-	app.get('/auth/facebook/callback/:verify_token',function(req,res){
-		var token="rebootkamp"
-		console.log(req.params.verify_token);
-		if(req.params.verify_token === token){
-			console.log('verifed');
-			res.status(200).send();
-		}else{
-			res.status(500).send("crash");
-		}
-	})
-
-	// app.get('/auth/facebook/callback/:hub.challenge',function(req,res){
-	// 	console.log("its a request 2");
-	// 	res.status(200).send();
-	// })
-
-	// app.get('/auth/facebook/callback/:hub.verify_token',function(req,res){
-	// 	var token="rebootkamp";
-	// 	console.log(req.params);
-
-	// 	res.status(200).send();
-	// })
+	app.get('/auth/facebook/callback', function(req, res) {
+	  if (req.query['hub.mode'] === 'subscribe' &&
+	      req.query['hub.verify_token'] === "rebootkamp") {
+	    console.log("Validating webhook");
+	    res.status(200).send(req.query['hub.challenge']);
+	  } else {
+	    console.error("Failed validation. Make sure the validation tokens match.");
+	    res.sendStatus(403);          
+	  }  
+	});
 }
