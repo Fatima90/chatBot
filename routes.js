b05fdf010235f58d2bc2a221d902db554cdb79e3
Fatima Hammami <en.f.h@live.com>
2017-04-12 15:54:55 +0300

    fix

--- routes.js
+++ routes.js
@@ -197,14 +197,14 @@ function sendStepsMessage(recipientId, messageText) {
 	          {
 	            title: "Step 3",
 	            subtitle: "keep track of Your Email for The next step",
-	            item_url: "http://i.imgur.com/46g2vyz.png",               
-	            image_url: "http://i.imgur.com/46g2vyz.png"
+	            item_url: "http://i.imgur.com/XwPNAY4.png",               
+	            image_url: "http://i.imgur.com/XwPNAY4.png"
 	          },
 	          {
 	            title: "Step 4",
 	            subtitle: "Fill The Applications",
-	            item_url: "http://i.imgur.com/OlXwIUm.png"               
-	          
+	            item_url: "http://i.imgur.com/OlXwIUm.png",
+	            image_url: "http://i.imgur.com/OlXwIUm.png"               
 	          }]
 	        }
 	      }
